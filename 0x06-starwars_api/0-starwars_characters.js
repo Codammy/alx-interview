1c234c99a03690dfea5892505c243996ec4b1236
codam <damisco005@gmail.com>
2024-11-16 13:43:56 +0100

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,40 +1,36 @@
 #!/usr/bin/node
-
+/**
+ * fetches character names of starwars api movies
+ */
 const request = require('request');
-
 const URL = 'https://swapi-api.alx-tools.com/api';
 const resource = '/films/';
 const id = process.argv[2];
 
-const uri = `${URL}${resource}${id || ''}`;
+const url = `${URL}${resource}${id || ''}`;
 
-request(uri, function (error, response, body) {
-  //   if (error) {
-  //     throw new Error(error);
-  //   }
-  if (error || response.statusCode !== 200) {
-    return;
+async function getCharacterNames(url) {
+  try {
+    const { body } = await fetch(url);
+    for (const character of body.characters) {
+      const { body } = await fetch(character);
+      console.log(body.name);
+    }
+  } catch (err) {
+    throw new Error(err);
   }
-  const data = JSON.parse(body);
-  let characters = [];
-  if (id) {
-    characters = data.characters;
-  } else {
-    data.results.forEach((movie) => {
-      characters = [...characters, ...movie.characters];
-    });
-  }
+}
 
-  //   console.log(characters)
-
-  characters.forEach((character) => {
-    request(character, function (error, response, body) {
-      if (error) {
-        // throw new Error(error);
-        return;
+function fetch(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        reject({ code: res.statusCode, err });
+      } else {
+        resolve({ code: res.statusCode, body: JSON.parse(body) });
       }
-      const ch = JSON.parse(body);
-      console.log(ch.name);
     });
   });
-});
+}
+
+getCharacterNames(url);
