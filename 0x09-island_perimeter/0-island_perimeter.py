52f286f2764255337230c22f7dcc88d424dd39ef
Mr Dami <damisco005@gmail.com>
2025-01-22 17:57:46 +0100

    fix pretty error

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,8 +2,10 @@
 """
 Function that calculates the perimeter of an island in a grid.
 
-The grid is represented as a list of lists, where 1 represents land and 0 represents water.
-The function calculates the perimeter by adding 4 for each land cell and subtracting 2 for each shared edge with another land cell.
+The grid is represented as a list of lists, where 1 represents
+land and 0 represents water. The function calculates the perimeter
+by adding 4 for each land cell and subtracting 2 for each shared edg
+e with another land cell.
 
 Args:
     grid (list of list of int): A 2D grid representing the island.
@@ -12,15 +14,17 @@ Returns:
     int: The perimeter of the island.
 """
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island in the given grid.
 
-    The perimeter is calculated by adding 4 for each land cell and subtracting 2 for each
-    shared edge between adjacent land cells.
+    The perimeter is calculated by adding 4 for each land cell
+    and subtracting 2 for each shared edge between adjacent land cells.
 
     Args:
-        grid (list of list of int): A 2D grid representing the island, with 1 for land and 0 for water.
+        grid (list of list of int): A 2D grid representing the island,
+        with 1 for land and 0 for water.
 
     Returns:
         int: The perimeter of the island.
